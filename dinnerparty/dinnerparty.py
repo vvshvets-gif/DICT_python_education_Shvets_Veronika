0554939c222f688af450b3dfaf66f5b379e67b47
Veronika <v.v.shvets@student.khai.edu>
2025-12-20 20:08:06 +0200

    Updated code

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,58 +1,76 @@
 # Етап 4: Фінальна версія з перерахунком
 import random
 
-# 1. Запитуємо кількість людей
-try:
-    num_friends = int(input("Enter the number of friends joining (including you):\n"))
-except ValueError:
-    num_friends = 0
+def get_number_of_friends() -> int:
+    try:
+        return int(input("Enter the number of friends joining (including you):\n"))
+    except ValueError:
+        return 0
 
-friends_dict = {}
+def get_total_amount() -> float:
+    try:
+        return float(input("Enter the total amount:\n"))
+    except ValueError:
+        return 0
 
-if num_friends <= 0:
-    print("No one is joining for the party")
-else:
-    print("Enter the name of every friend (including you), each on a new line:")
+def get_friends_dict(num_friends: int):
+    friends_dict = {}
     for _ in range(num_friends):
         name = input()
         friends_dict[name] = 0
 
-    # 2. Зчитуємо загальну суму
-    try:
-        total_amount = float(input("Enter the total amount:\n"))
-    except ValueError:
-        total_amount = 0
+    return friends_dict
 
-    # 3. Запитуємо, чи потрібен "щасливчик"
+def get_lucky_one(friends_dict):
+    return random.choice(list(friends_dict.keys()))
+
+def calculate_bill_with_lucky(lucky_one, friends_dict, total_amount):
+    num_friends = len(friends_dict)
+    if num_friends > 1:
+        new_num_friends = num_friends - 1
+        new_split_amount = total_amount / new_num_friends
+
+        # 6. Округлення
+        new_rounded_amount = round(new_split_amount, 2)
+
+        # 7. Оновлення словника
+        for name in friends_dict:
+            if name == lucky_one:
+                friends_dict[name] = 0.00  # Щасливчик платить 0
+            else:
+                friends_dict[name] = new_rounded_amount
+    elif num_friends == 1:
+        # Якщо тільки 1 людина, вона платить все, і не може бути "щасливчиком" у сенсі поділу
+        friends_dict[lucky_one] = round(total_amount, 2)
+
+def start():
+    # 1. Запитуємо кількість людей
+    num_friends = get_number_of_friends()
+
+    # 1.1 Виходимо з програми, якщо число меьше 1
+    if num_friends <= 0:
+        print("No one is joining for the party")
+        return
+
+    # 2. Зчитуємо імена друзів
+    print("Enter the name of every friend (including you), each on a new line:")
+    friends_dict = get_friends_dict(num_friends)
+
+    # 3. Зчитуємо загальну суму
+    total_amount = get_total_amount()
+
+    # 4. Запитуємо, чи потрібен "щасливчик"
     lucky_choice = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n').lower()
 
-    lucky_one = None
-
     if lucky_choice == 'yes':
-        # 4. Випадковий вибір імені
-        lucky_one = random.choice(list(friends_dict.keys()))
+        # 4.1 Випадковий вибір імені
+        lucky_one = get_lucky_one(friends_dict)
         print(f"{lucky_one} is the lucky one!")
 
         # --- ЛОГІКА ПЕРЕРАХУНКУ ЕТАПУ 4 ---
 
         # 5. Перерахунок на n - 1 людей
-        if num_friends > 1:
-            new_num_friends = num_friends - 1
-            new_split_amount = total_amount / new_num_friends
-
-            # 6. Округлення
-            new_rounded_amount = round(new_split_amount, 2)
-
-            # 7. Оновлення словника
-            for name in friends_dict:
-                if name == lucky_one:
-                    friends_dict[name] = 0.00 # Щасливчик платить 0
-                else:
-                    friends_dict[name] = new_rounded_amount
-        elif num_friends == 1:
-             # Якщо тільки 1 людина, вона платить все, і не може бути "щасливчиком" у сенсі поділу
-             friends_dict[lucky_one] = round(total_amount, 2)
-
+        calculate_bill_with_lucky(lucky_one, friends_dict, total_amount)
     else:
         # Якщо користувач не вибрав "щасливчика" (No або інше)
         print("No one is going to be lucky")
@@ -66,3 +84,5 @@ else:
 
     # 9. Виведення фінального словника
     print(friends_dict)
+
+start()
