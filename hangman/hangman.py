47b8f5de88473a81ec5c52cfe9e4841de6bea66f
Veronika <v.v.shvets@student.khai.edu>
2025-12-23 16:01:14 +0200

    Final w/ docstring

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,18 +1,44 @@
-# Етап 8 - ФІНАЛЬНА ВЕРСІЯ
+"""
+Модуль для запуску гри 'Шибениця' (Hangman).
+
+Цей скрипт ініціалізує ігровий процес, налаштовує обробник введення-виведення
+та керує головним меню гри.
+"""
+
 from IO import ConsoleIO
 from game import Game
 
-console_io = ConsoleIO()
-game = Game(words=['python', 'java', 'javascript', 'php'], attempts_left=8, io_handler=console_io)
 
-console_io.print_output("HANGMAN")
+def main():
+    """
+    Головна функція, яка керує життєвим циклом програми.
 
-while True:
-    menu_choice = console_io.get_input('Type "play" to play the game, "exit" to quit: ').lower()
+    Створює екземпляри ConsoleIO та Game, відображає вітальне повідомлення
+    та обробляє вибір користувача в меню (грати або вийти).
+    """
+    console_io = ConsoleIO()
 
-    if menu_choice == 'play':
-        game.start()
-    elif menu_choice == 'exit':
-        break
-    else:
-        continue
+    # Ініціалізація гри з набором слів та кількістю спроб
+    game = Game(
+        words=['python', 'java', 'javascript', 'php'],
+        attempts_left=8,
+        io_handler=console_io
+    )
+
+    console_io.print_output("--- HANGMAN ---")
+
+    while True:
+        menu_choice = console_io.get_input('Type "play" to play the game, "exit" to quit: ').lower()
+
+        if menu_choice == 'play':
+            game.start()
+        elif menu_choice == 'exit':
+            console_io.print_output("Goodbye!")
+            break
+        else:
+            # Якщо введення некоректне, цикл просто повторюється
+            continue
+
+
+if __name__ == "__main__":
+    main()
