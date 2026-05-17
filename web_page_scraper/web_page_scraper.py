d26f5eda004349e82d820eb30384886eab73dc82
Veronika Svets <v.v.shvets@student.khai.edu>
2026-05-17 12:32:25 +0300

    Added stage 3

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -1,30 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
-import json
-import re
-
-HEADERS = {'Accept-Language': 'en-US,en;q=0.5'}
 
 url = input("Input the URL:\n> ")
 
-if 'title' not in url:
-    print("Invalid movie page!")
+response = requests.get(url)
+
+if response.status_code != 200:
+    print(f"The URL returned {response.status_code}!")
 else:
-    response = requests.get(url, headers=HEADERS)
-
-    if response.status_code != 200:
-        print("Invalid movie page!")
-    else:
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        title_tag = soup.find('title')
-        description_tag = soup.find('meta', {'name': 'description'})
-
-        if not title_tag or not description_tag:
-            print("Invalid movie page!")
-        else:
-            # strip " (year) - IMDb" suffix from title
-            title = re.sub(r'\s*\(\d{4}\).*$', '', title_tag.text.strip())
-            description = description_tag['content'].strip()
-
-            print(json.dumps({"title": title, "description": description}))
+    with open("source.html", "wb") as f:
+        f.write(response.content)
+    print("Content saved.")
