428b93be7f3d09ad36010ed1dca9006d51ed9e75
Veronika Svets <v.v.shvets@student.khai.edu>
2026-05-17 12:37:25 +0300

    Added stage 1

---
+++ currency_exchange/currency_exchange.py
@@ -0,0 +1,4 @@
+mycoins = float(input("Please, enter the number of mycoins you have: "))
+rate = float(input("Please, enter the exchange rate: "))
+
+print(f"The total amount of dollars: {mycoins * rate:.2f}")
