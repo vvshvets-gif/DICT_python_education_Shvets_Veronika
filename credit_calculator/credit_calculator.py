ee7baf725d368bd895bb63a3ef1d3f876c1e88e4
Veronika Svets <v.v.shvets@student.khai.edu>
2026-05-17 11:53:37 +0300

    Added stage 2

--- credit_calculator/credit_calculator.py
+++ credit_calculator/credit_calculator.py
@@ -1,5 +1,20 @@
-print("Loan principal: 1000")
-print("Month 1: repaid 250")
-print("Month 2: repaid 250")
-print("Month 3: repaid 500")
-print("The loan has been repaid!")
+import math
+
+principal = int(input("Enter the loan principal:\n> "))
+
+print('What do you want to calculate?\ntype "m" – for number of monthly payments,\ntype "p" – for the monthly payment:')
+choice = input("> ")
+
+if choice == "m":
+    payment = int(input("Enter the monthly payment:\n> "))
+    months = math.ceil(principal / payment)
+    print(f"It will take {months} month{'s' if months > 1 else ''} to repay the loan")
+
+elif choice == "p":
+    months = int(input("Enter the number of months:\n> "))
+    payment = math.ceil(principal / months)
+    last_payment = principal - (months - 1) * payment
+    if last_payment == payment:
+        print(f"Your monthly payment = {payment}")
+    else:
+        print(f"Your monthly payment = {payment} and the last payment = {last_payment}.")
