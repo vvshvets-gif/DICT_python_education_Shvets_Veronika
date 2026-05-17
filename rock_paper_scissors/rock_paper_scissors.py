28e2800db4440642d978c53d78cfda2ec4a3a8ed
Veronika Svets <v.v.shvets@student.khai.edu>
2026-05-17 11:34:09 +0300

    Added stage 2

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,9 +1,17 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
 user = input("> ")
+computer = random.choice(choices)
 
-win_map = {
-    "rock": "paper",
-    "paper": "scissors",
-    "scissors": "rock"
-}
-
-print(f"Sorry, but the computer chose {win_map[user]}")
+if user == computer:
+    print(f"There is a draw ({computer})")
+elif (
+    (user == "rock" and computer == "scissors") or
+    (user == "scissors" and computer == "paper") or
+    (user == "paper" and computer == "rock")
+):
+    print(f"Well done. The computer chose {computer} and failed")
+else:
+    print(f"Sorry, but the computer chose {computer}")
