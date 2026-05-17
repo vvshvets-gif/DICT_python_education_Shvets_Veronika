0d6eb9a3397105a9094b66025671292ed5501c2f
Veronika Svets <v.v.shvets@student.khai.edu>
2026-05-17 12:40:41 +0300

    Added stage 4

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -1,9 +1,28 @@
 import requests
 
-currency = input("Enter the currency code: ").lower()
+base = input("> ").lower()
 
-response = requests.get(f"http://www.floatrates.com/daily/{currency}.json")
-data = response.json()
+data = requests.get(f"http://www.floatrates.com/daily/{base}.json").json()
 
-print(data["usd"])
-print(data["eur"])
+cache = {
+    "usd": data["usd"]["rate"],
+    "eur": data["eur"]["rate"],
+}
+
+while True:
+    target = input("> ").lower()
+    if not target:
+        break
+
+    amount = float(input("> "))
+
+    print("Checking the cache...")
+
+    if target not in cache:
+        print("Sorry, but it is not in the cache!")
+        cache[target] = data[target]["rate"]
+    else:
+        print("It is in the cache!")
+
+    result = round(amount * cache[target], 2)
+    print(f"You received {result} {target.upper()}.")
