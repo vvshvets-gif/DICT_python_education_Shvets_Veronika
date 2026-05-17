29b1c8b09a261559f0969eef78afb10879e537eb
Veronika Svets <v.v.shvets@student.khai.edu>
2026-05-17 12:38:15 +0300

    Added stage 2

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -1,4 +1,12 @@
-mycoins = float(input("Please, enter the number of mycoins you have: "))
-rate = float(input("Please, enter the exchange rate: "))
+RATES = {
+    "ARS": 0.82,
+    "HNL": 0.17,
+    "AUD": 1.9622,
+    "MAD": 0.208,
+}
 
-print(f"The total amount of dollars: {mycoins * rate:.2f}")
+mycoins = float(input("> "))
+
+for currency, rate in RATES.items():
+    amount = round(mycoins * rate, 2)
+    print(f"I will get {amount} {currency} from the sale of {mycoins} mycoins.")
